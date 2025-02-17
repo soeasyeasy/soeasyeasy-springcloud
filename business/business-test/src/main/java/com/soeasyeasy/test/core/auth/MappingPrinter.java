b2417b2ceffabc786b132eaf4dab46fc71c5d112
huangcheng <2387020215@qq.com>
2025-02-17 14:51:43 +0800

    init

---
+++ business/business-test/src/main/java/com/soeasyeasy/test/core/auth/MappingPrinter.java
@@ -0,0 +1,33 @@
+package com.soeasyeasy.test.core.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.Map;
+
+@Component
+@Slf4j
+public class MappingPrinter implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        // 获取所有@RequestMapping映射关系
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = this.requestMappingHandlerMapping.getHandlerMethods();
+
+        handlerMethods.forEach((mapping, method) -> {
+            log.info("Mapped URL path: " + mapping);
+            log.info("Controller Method: " + method.getMethod().getName());
+            log.info("Controller Class: " + method.getBeanType().getName());
+            log.info("-----------------------------");
+        });
+    }
+}
