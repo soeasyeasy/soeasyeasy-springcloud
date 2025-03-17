f1611f7e16c7a47c52da098d8643dc052699d2c1
huangcheng <2387020215@qq.com>
2025-03-17 17:58:42 +0800

    集成进mapstruct

--- business/business-test/src/main/java/com/soeasyeasy/test/entity/DO/ProductDO.java
+++ business/business-test/src/main/java/com/soeasyeasy/test/entity/DO/ProductDO.java
@@ -1,7 +1,6 @@
 package com.soeasyeasy.test.entity.DO;
 
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
@@ -10,7 +9,6 @@ import org.springframework.data.annotation.Id;
  */
 @Data
 @TableName("tb_product")
-@AllArgsConstructor
 public class ProductDO {
     @Id
     private String id;
--- business/business-test/src/main/java/com/soeasyeasy/test/entity/DTO/ProductDTO.java
+++ business/business-test/src/main/java/com/soeasyeasy/test/entity/DTO/ProductDTO.java
@@ -1,16 +1,12 @@
 package com.soeasyeasy.test.entity.DTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
 
 /**
  * @author hc
  */
 @Data
-@Document(indexName = "products")
-@AllArgsConstructor
 public class ProductDTO {
     @Id
     private String id;
